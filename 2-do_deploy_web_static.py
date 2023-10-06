44eeca89df3b2febe7b69d7227c4b6efd453c190
joshpee22 <owobujoshua2017@gmail.com>
2023-10-05 20:41:12 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,47 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+"""Deploy an archive of static html to my web servers with Fabric3"""
 
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric import api
+from fabric.contrib import files
+from fabric.exceptions import CommandExecutionError
+import os
 
-env.hosts = ['3.235.178.105', '3.235.75.111']
+
+api.env.hosts = ['34.227.94.33', '54.90.37.150']
+api.env.user = 'ubuntu'
+api.env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
+    """Transfer `archive_path` to web servers.
+    
     Args:
-        archive_path (str): The path of the archive to distribute.
+        archive_path (str): Path of the .tgz file to transfer.
+        
     Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
+        bool: True on success, False otherwise.
     """
-    if os.path.isfile(archive_path) is False:
+    if not os.path.isfile(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    with api.cd('/tmp'):
+        basename = os.path.basename(archive_path)
+        root, ext = os.path.splitext(basename)
+        outpath = '/data/web_static/releases/{}'.format(root)
+        try:
+            putpath = api.put(archive_path)
+            if files.exists(outpath):
+                api.run('rm -rdf {}'.format(outpath))
+            api.run('mkdir -p {}'.format(outpath))
+            api.run('tar -xzf {} -C {}'.format(putpath[0], outpath))
+            api.run('rm -f {}'.format(putpath[0]))
+            api.run('mv -u {}/web_static/* {}'.format(outpath, outpath))
+            api.run('rm -rf {}/web_static'.format(outpath))
+            api.run('rm -rf /data/web_static/current')
+            api.run('ln -sf {} /data/web_static/current'.format(outpath))
+            api.puts('New version deployed!')
+        except CommandExecutionError as e:
+            api.puts('Deployment failed: {}'.format(str(e)))
+            return False
+        else:
+            return True
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
