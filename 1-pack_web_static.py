b7641d1e2d26fe485660d17c1a2703a69291b69a
joshpee22 <owobujoshua2017@gmail.com>
2023-10-09 04:58:26 -0700

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,26 +1,27 @@
-#!/usr/bin/python3
-'''Fabric script to generate .tgz archive'''
-
 from fabric.api import local
-from datetime import datetime
+from time import strftime
 
-from fabric.decorators import runs_once
-
-@runs_once
 def do_pack():
-    '''
-    Generates .tgz archive from the contents of the web_static folder.
+    date = strftime("%Y%m%d%H%M%S")
+    try:
+        print("Creating versions directory...")
+        local("mkdir -p versions")
 
-    Returns:
-        str: Archive path if successful, None otherwise.
-    '''
-    local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz"
-            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
+        print("Creating .tgz archive...")
+        result = local("tar -cvzf versions/web_static_{}.tgz web_static".format(date), capture=True)
 
-    if result.failed:
+        if result.succeeded:
+            archive_path = "versions/web_static_{}.tgz".format(date)
+            print("Archive created successfully:", archive_path)
+            return archive_path
+        else:
+            print("Error creating archive.")
+            return None
+
+    except Exception as e:
+        print("Error:", e)
         return None
-    return path
+
+# Run the script
+do_pack()
 
---
+++ run_fab.py
@@ -0,0 +1,4 @@
+from 1-pack_web_static import do_pack
+
+if __name__ == "__main__":
+    do_pack()
